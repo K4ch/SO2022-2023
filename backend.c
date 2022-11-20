20b468d145f1d7bb6c2f23a965f2b9beb0ffe5dd
CarlosSilva2002 <95040939+CarlosSilva2002@users.noreply.github.com>
2022-11-20 23:24:00 +0000

    Add files via upload

--- backend.c
+++ backend.c
@@ -1,32 +1,59 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include <unistd.h>
 #include "utils.h"
+#include "users_lib.h"
+
+bool checkPointerNull(char *ptr){
+        if(ptr == NULL){
+                printf("Argumentos a mais\n");
+                return true;
+        }
+        return false;
+}
+
 
 void main(int argc, char ** argv){
-	int pipePromotor[2];
-	pipe(pipePromotor);
-	int pid = fork();
 
-	if(pid < 0){
-		return;
-        }else if(pid == 0){
-		close(pipePromotor[0]);
-		close(STDOUT_FILENO);
-		dup(pipePromotor[1]);
-		close(pipePromotor[1]);
+	char input[100];
+	bool end = false;
+	int initSize = strlen(input);
+        char delim[] = " ";
 
-		execl("./promotor_oficial" , "./promotor_oficial", "NULL");
-		perror("Ficheiro não encontrado\n");
-	}else{
-		char mensagem[200];
-		close(pipePromotor[1]);
-		while(1){
-			read(pipePromotor[0], &mensagem, sizeof(mensagem));
-			printf("%s\n", &mensagem);
+       while(!end){
+                fgets(input, 100, stdin);
+                char *ptr = strtok(input, delim);
+		if(strcmp(ptr, "users\n")==0){
+			printf("Comando \"users\" recebido\n");
+		}else if(strcmp(ptr, "list\n")==0){
+                        printf("Comando \"list\" recebido\n");
+		}else if(strcmp(ptr, "kick")==0){
+                        ptr = strtok(NULL, delim);
+                        if(checkPointerNull(ptr)){
+                                continue;
+                        }
+			char username[20];
+			strcpy(username, ptr);
+                        printf("Comando \"kick\" recebido\n");
+                }else if(strcmp(ptr, "prom\n")==0){
+                        printf("Comando \"prom\" recebido\n");
+                }else if(strcmp(ptr, "reprom\n")==0){
+                        printf("Comando \"reprom\" recebido\n");
+                }else if(strcmp(ptr, "cancel")==0){
+                        ptr = strtok(NULL, delim);
+                        if(checkPointerNull(ptr)){
+                                continue;
+                        }
+                        char nomeExec[20];
+                        strcpy(nomeExec, ptr);
+                        printf("Comando \"cancel\" recebido\n");
+                }else if(strcmp(ptr, "close\n")==0){
+                        printf("Comando \"reprom\" recebido\n");
+                }else{
+			printf("Comando inválido\n");
 		}
+		printf("\n\n");
 	}
-
 }
